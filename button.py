0af429c98c29683c195f19d20c911f780bc9abf3
Michael Elsdörfer <michael@elsdoerfer.info>
2018-04-23 21:35:55 +0100

    Make it work without GPIO.

--- button.py
+++ button.py
@@ -1,15 +1,45 @@
-import RPi.GPIO as GPIO
+try:
+    import RPi.GPIO as GPIO
+except ImportError:
+    GPIO = None
 import time
 
+
+LED_PIN = 5
+BUTTON_PIN = 23
+
 def setup():
+    if not GPIO:
+        return
+        
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    # The button input
+    GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    # The LED output
+    GPIO.setup(LED_PIN, GPIO.OUT)
+
 
 def query():
+    if not GPIO:
+        return
+
     state = GPIO.input(23)
     if state == False:
         return True
     return False
 
+
+def set_led(on):
+    if not GPIO:
+        return
+
+    GPIO.output(LED_PIN, on)
+
+
 def cleanup():
+    if not GPIO:
+        return
+    
     GPIO.cleanup()
